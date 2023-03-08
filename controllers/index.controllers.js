0d78fc099ed5d01668420619ba7f30fa3e9a3f65
HamzaSajjad141 <hamzasajjad1277@gmail.com>
2023-03-08 11:30:29 +0500

    Signup,messAttendance,gatepass,roomAllotment

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -22,3 +22,25 @@ exports.forgetpassword = function(req,res)
 {
     res.send("Forget Password Page");
 }
+
+//-------------------HAMZA SAJJAD PART----------------------------//
+exports.getSignUp = function(req,res)
+{
+    res.send("Sign Up Page")
+}
+
+exports. getRoomAllotment = function(req,res)
+{
+    res.send("Room Allotment Page")
+}
+
+exports. getMessAttendance = function(req,res)
+{
+    res.send("Mess Attendance Page")
+}
+
+exports. getGatepass = function(req,res)
+{
+    res.send("Gate Pass Page")
+}
+
--- routers/index.routers.js
+++ routers/index.routers.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const {Home,AboutUs,ContactUs,LogIn,forgetpassword} = require("../controllers/index.controllers");
 
+const {getSignUp,getRoomAllotment,getMessAttendance,getGatepass} = require("../controllers/index.controllers");
+
 router.get("/home",(req,res) =>
 {
   Home(req,res);
@@ -27,4 +29,29 @@ router.get("/login/forgetpassword",(req,res) =>
 {
   forgetpassword(req,res);
 })
+
+//--------HAMZA SAJJAD PART--------------//
+router.get("/signup", (req,res) =>
+  {
+    getSignUp(req,res);
+  }
+)
+
+router.get("/Room", (req,res) =>
+  {
+    getRoomAllotment(req,res);
+  }
+)
+
+router.get("/Mess", (req,res) =>
+  {
+    getMessAttendance(req,res);
+  }
+)
+
+router.get("/GatePass", (req,res) =>
+  {
+    getGatepass (req,res);
+  }
+)
 module.exports = router;
